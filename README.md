6f12bf5a28e41acef243fa5fa191687ba337a7d9
LocBDinh <108839361+LocBDinh@users.noreply.github.com>
2024-03-20 23:40:10 -0400

    Stock Prediction Model Training
    Setup model training using yahoo finance library.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Stock Prediction Model
+
+This is a stock prediction model built using the YahooFinance library. The model aims to predict the future price movements of stocks based on historical and real time data. Utilized _Long Short-Term Memory(LSTM)_ which is a _Recurrent Neural Network(RNN)_ architecture.
+
+## Installation
+
+#### To use this stock prediction model, you need to have the following libraries installed. You can install it using pip:
+ 1. _Scikit-Learn_
+ 2. _TensorFlow and Keras_
+ 3. _Pandas_
+ 4. _Numpy_
+ 5. _Matplotlib_
+
+
---
+++ main.py
@@ -0,0 +1,158 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime as dt
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn import metrics
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM
+
+# Prompt the user to enter the stock ticker
+ticker = input("Enter the stock ticker: ").upper()
+
+# Set the start and end date
+start_year = "2000"
+end = str(dt.date.today())
+
+# Download historical data for required stocks
+data = yf.download(ticker, start=(start_year + "-01-01"), end = end, 
+                   auto_adjust = True) # Auto_adjust automatically adjusts the stock prices for splits and dividends
+
+# Get the most recent 5 days of data
+print(data.tail())
+
+# Plot the adjusted close price
+stock = yf.Ticker(ticker)
+
+# Formatted stock info
+"""
+for key, value in stock.info.items():
+     print(key, ':', value)
+"""
+
+# Show User the stock info
+print("\nStock Information:")
+print("------------------")
+print(f"Name: {stock.info['shortName']}")
+print(f"Ticker: {ticker}")
+print(f"Open: {stock.info['open']}")
+print(f"Previous Close: {stock.info['previousClose']}")
+print(f"Volume: {stock.info['volume']}")
+
+# Not all stocks have all information
+try:
+    print(f"Diviend Yield: {stock.info['dividendYield']}")
+except:
+    print("Dividend Yield: N/A")
+try:
+    print(f"Market Cap: {stock.info['marketCap']}")
+except:
+    print("Market Cap: N/A")
+try:
+    print(f"52 Week High: {stock.info['fiftyTwoWeekHigh']}")
+    print(f"52 Week Low: {stock.info['fiftyTwoWeekLow']}")
+except:
+    print("52 Week High: N/A")
+    print("52 Week Low: N/A")
+try:
+    print(f"PE Ratio: {stock.info['trailingPE']}")
+except:
+    print("PE Ratio: N/A")
+try:
+    print(f"Earnings Per Share: {stock.info['trailingEps']}")
+except:
+    print("Earnings Per Share: N/A")
+
+# Store the most recent 5 day data
+recent_df = yf.download(ticker, period = "5d") 
+
+# Store Dividend Yield [Display] and Stock Split History [Display]
+dividends = stock.dividends
+splits = stock.splits
+
+# Displays 5 year chart of stock
+data = stock.history(period = '5y' )
+data['Close'].plot()
+plt.title(f"{ticker} Stock Prices")
+plt.show()
+
+# Inserts the data into a pandas dataframe
+data = yf.download(ticker)[['Adj Close']]
+data.reset_index(inplace = True)
+data.drop('Date', axis = 1, inplace = True)
+
+# Split the data into training and testing data
+split_percent = 0.90
+split_point = round(len (data) * split_percent)
+train_data = data.iloc[:split_point]
+test_data = data.iloc[split_point:]
+
+# Using Scikit-Learn to preprocess data before feeding it into the model
+scaler = MinMaxScaler()
+scaler.fit(train_data)
+scaled_train = scaler.transform(train_data)
+scaled_test = scaler.transform(test_data)
+
+# Description: This file contains the functions for preprocessing the time series data
+def timeseries_preprocessing(scaled_train, scaled_test, lags):
+    X,Y = [],[]
+    for t in range(len(scaled_train) - lags - 1):
+        X.append(scaled_train[t:(t + lags), 0])
+        Y.append(scaled_train[(t + lags), 0])
+
+    Z,W = [],[]
+    for t in range(len(scaled_test) - lags - 1):
+        Z.append(scaled_test[t:(t + lags), 0])
+        W.append(scaled_test[(t + lags), 0])
+
+    X_train, Y_train, X_test, Y_test = np.array(X), np.array(Y), np.array(Z), np.array(W)
+
+    X_train = X_train.reshape((X_train.shape[0],X_train.shape[1],1))
+    X_test = X_test.reshape((X_test.shape[0],X_test.shape[1],1))
+
+    return X_train, Y_train, X_test, Y_test
+
+# Preprocess the data
+X_train, Y_train, X_test, Y_test = timeseries_preprocessing(scaled_train, scaled_test, 10)
+
+# Create the model using Keras
+model = Sequential()
+model.add(LSTM(256,input_shape = (X_train.shape[1], 1)))
+model.add(Dense(1))
+model.compile(optimizer = 'adam', loss = 'mse')
+
+# Train the model on the training data with 10 epochs [Should be changed to 1000+ for better results]
+history = model.fit(x=X_train,y=Y_train,epochs=10,validation_data=(X_test,Y_test),shuffle=False)
+
+# Plot the loss and validation loss
+axes = plt.axes()
+axes.plot(pd.DataFrame(model.history.history)['loss'], label = 'Loss')
+axes.plot(pd.DataFrame(model.history.history)['val_loss'], label = 'Validation Loss')
+axes.legend(loc=0)
+axes.set_title('Model fitting performance')
+
+# Predict the stock prices using the model
+Y_predicted=scaler.inverse_transform(model.predict(X_test))
+Y_true=scaler.inverse_transform(Y_test.reshape(Y_test.shape[0],1))
+
+# Plot the predicted stock prices vs the true stock prices from data
+axes=plt.axes()
+axes.plot(Y_true, label='True Y')
+axes.plot(Y_predicted, label='Predicted Y')
+axes.legend(loc=0)
+axes.set_title('Prediction adjustment')
+
+# Calculate the metrics for the model
+print("")
+print("Overall Model Performance:")
+print("Mean Absolute Error:", metrics.mean_absolute_error(Y_true, Y_predicted))
+print("Mean Squared Error:", metrics.mean_squared_error(Y_true, Y_predicted))
+print("Root Mean Squared Error:", np.sqrt(metrics.mean_squared_error(Y_true, Y_predicted)))
+print("R2 Score:", metrics.r2_score(Y_true, Y_predicted))
+print("------------------")
+print("Model Accuracy:")
+Y_predicted = scaler.inverse_transform(model.predict(X_train))
+Y_true = scaler.inverse_transform(Y_train.reshape(Y_train.shape[0], 1))
+print((1 - (metrics.mean_absolute_error(Y_true, Y_predicted) / Y_true.mean())) * 100)
