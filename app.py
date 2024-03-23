49992d77197adc5c9f69a4910dbf378779ed199c
LocBDinh <108839361+LocBDinh@users.noreply.github.com>
2024-03-22 21:54:13 -0400

    Implement Flask, HTML pages, and CORS
    Implemented Flask and CORS for future resource sharing with third party api's. Created html pages for home and predictions. Need to implement the model.

--- app.py
+++ app.py
@@ -1,47 +1,23 @@
-import flask
-from flask import request, jsonify
-import yfinance as yf
-import json
-import requests
-import os
+from flask import Flask, request, render_template
+from flask_cors import CORS, cross_origin
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
+cors = CORS(app)
 
-@app.route('/api/v1/health', methods=['GET'])
+@app.route('/', methods=['GET'])
+@cross_origin()
 def status():
-    return jsonify({'status': 'UP'})
+    return render_template('index.html')
 
-@app.route('/api/v1/echo', methods=['POST'])
-def echo():
-    data = request.get_json()
-    return jsonify(data)
-
-@app.route('/api/v1/stock', methods=['GET'])
-def stock():
-    ticker = request.args.get('ticker')
-    data = yf.Ticker(ticker)
-    return jsonify(data.info)
-
-@app.route('/api/v1/stock', methods=['POST'])
-def stock_post():
-    data = request.get_json()
-    ticker = data['ticker']
-    stock = yf.Ticker(ticker)
-    return jsonify(stock.info)
-
-@app.route('/api/v1/stock', methods=['PUT'])
-def stock_put():
-    data = request.get_json()
-    ticker = data['ticker']
-    stock = yf.Ticker(ticker)
-    return jsonify(stock.info)
-
-@app.route('/api/v1/stock', methods=['DELETE'])
-def stock_delete():
-    data = request.get_json()
-    ticker = data['ticker']
-    stock = yf.Ticker(ticker)
-    return jsonify(stock.info)
+@app.route('/stock-predictions', methods=['POST', 'GET'])
+@cross_origin()
+def stock_predictions():
+    try:
+        # NOTE: Need to tokenize the ticker input and call the model
+        ticker = request.form.get['ticker']
+    except:
+        return 'Model could not be found.'
+    return render_template('predictions.html', ticker=ticker)
 
 if __name__ == '__main__':
-    pass
+    app.run(debug=True, host='', port=1337) # Debug mode is on and allows for reloading the server when changes are made
